5f854c48180ac51a3c4d97f0f12d5990bfb6d5ee
regina.sadrieva.it@gmail.com <regina.sadrieva.it@gmail.com>
2025-11-26 19:50:33 +0900

    adding workflow, json edit for workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/0-REACT-TO-DO/',
+  base: '/0-REACT-TO-DO/', //key for GitHub Pages subpath
 });
